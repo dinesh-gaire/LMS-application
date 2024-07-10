c316487320799ada499084886f114cbd74fc0f07
dinesh-gaire <gairedinesh132@gmail.com>
2024-07-10 22:43:36 +0545

    db connected

--- server/server.ts
+++ server/server.ts
@@ -2,9 +2,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import { app } from "./app";
+import connectDB from './utils/db';
 const PORT = process.env.PORT;
 
 //create server
 app.listen(PORT, () => {
   console.log(`Server is connected with port ${PORT}`);
+  connectDB();
 });
