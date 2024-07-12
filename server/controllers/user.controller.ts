ab0bd56c07bc4724d33cf7bf630ac7a82207bffc
dinesh-gaire <gairedinesh132@gmail.com>
2024-07-12 21:41:30 +0545

    create activation token functionality added

---
+++ server/controllers/user.controller.ts
@@ -0,0 +1,35 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import { Request, Response, NextFunction } from "express";
+import userModel, { IUser } from "../models/user.model";
+import ErrorHandler from "../utils/ErrorHandler";
+import { CatchAsyncErrors } from "../middleware/catchAsyncErrors";
+import jwt, { Secret } from "jsonwebtoken";
+import ejs from "ejs";
+import path from "path";
+import sendMail from "../utils/sendMail";
+
+//create activation token
+
+interface IActivationToken {
+  token: string;
+  activationCode: string;
+}
+
+export const createActivationToken = (user: any): IActivationToken => {
+  const activationCode = Math.floor(1000 + Math.random() * 9000).toString();
+
+  const token = jwt.sign(
+    {
+      user,
+      activationCode,
+    },
+    process.env.ACTIVATION_SECRET as Secret,
+    {
+      expiresIn: "5m",
+    }
+  );
+
+  return { token, activationCode };
+};
